053d1fe457cc93de827b5ee34e5958cbd1c990d1
Rohan Sidhu <rohan.sidhu@outlook.com>
2022-08-31 23:51:37 +0530

    Putting Online

---
+++ new-header.php
@@ -0,0 +1,385 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header</title>
+    <link rel="stylesheet" href="new-header.css">
+    <script src="https://kit.fontawesome.com/f157c27a78.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <header class="header-laptop">
+        <nav class="top-nav">
+            <ul class="top-menu">
+                <a href="#">
+                    <li>Downloads</li>
+                </a>
+                <a href="#">
+                    <li>Customer Portal</li>
+                </a>
+                <a href="#">
+                    <li>Supplier Portal</li>
+                </a>
+                <a href="#">
+                    <li>Outlet</li>
+                </a>
+                <a href="#" style="background-color: #f1f1f1; font-size: 12.5px; border: none; padding: 0.15rem 1rem;">
+                    <li class="nav-link" style="display: flex; gap: 0.15rem; align-items: center;">
+                        <div class="flag-img">
+                            <img src="https://www.klopman.com/wp-content/plugins/sitepress-multilingual-cms/res/flags/en.png" alt="" class="flag-img">
+                        </div>
+                        ENGLISH
+                    </li>
+                </a>
+                <a href="#" style="border: none;">
+                    <li><i class="fa fa-user"></i></li>
+                </a>
+                <a href="#" style="border: none;">
+                    <li><i class="fa fa-search"></i></li>
+                </a>
+            </ul>
+        </nav>
+        <nav class="main-nav">
+            <img src="assets/img/logo-color.png" alt="" class="logo">
+            <ul class="main-menu">
+                <li class="nav-link">
+                    CORPORATE<i class="fa fa-plus"></i>
+                    <div class="hover-detail">
+                        <div class="details-container">
+                            <ul class="hover-menu">
+                                <a href="#" class="heading">
+                                    <li>about us</li>
+                                </a>
+                                <a href="#">
+                                    <li>history</li>
+                                </a>
+                                <a href="#">
+                                    <li>our plants</li>
+                                </a>
+                                <a href="#">
+                                    <li>manufacturing and certification</li>
+                                </a>
+                                <a href="#">
+                                    <li>management</li>
+                                </a>
+                                <a href="#">
+                                    <li>out partners</li>
+                                </a>
+                                <a href="#">
+                                    <li>careers</li>
+                                </a>
+                                <a href="#" style="color: #ED1C24;">
+                                    <li>careers</li>
+                                </a>
+                            </ul>
+                            <img src="https://www.klopman.com/wp-content/uploads/2018/10/mega_menu_corporate_img-1-286x300.jpg" class="details-img" alt="">
+                        </div>
+                    </div>
+                </li>
+                <li class="nav-link">
+                    FABRICS<i class="fa fa-plus"></i>
+                </li>
+                <li class="nav-link">
+                    INDUSTRIES<i class="fa fa-plus"></i>
+                    <div class="hover-detail">
+                        <div class="details-container" style="justify-content: start;">
+                            <ul class="hover-menu">
+                                <a href="#">
+                                    <li>our commitment</li>
+                                </a>
+                                <a href="#">
+                                    <li>environment</li>
+                                </a>
+                                <a href="#">
+                                    <li>sustainable fabrics</li>
+                                </a>
+                                <a href="#">
+                                    <li>people</li>
+                                </a>
+                            </ul>
+                            <ul class="hover-menu">
+                                <a href="#">
+                                    <li>our commitment</li>
+                                </a>
+                                <a href="#">
+                                    <li>environment</li>
+                                </a>
+                                <a href="#">
+                                    <li>sustainable fabrics</li>
+                                </a>
+                                <a href="#">
+                                    <li>people</li>
+                                </a>
+                            </ul>
+                            <ul class="hover-menu">
+                                <a href="#">
+                                    <li>our commitment</li>
+                                </a>
+                                <a href="#">
+                                    <li>environment</li>
+                                </a>
+                                <a href="#">
+                                    <li>sustainable fabrics</li>
+                                </a>
+                                <a href="#">
+                                    <li>people</li>
+                                </a>
+                            </ul>
+                        </div>
+                    </div>
+                </li>
+                <li class="nav-link">
+                    <span>SUSTAINABILITY</span><i class="fa fa-plus"></i>
+                    <div class="hover-detail">
+                        <div class="details-container">
+                            <ul class="hover-menu">
+                                <a href="#">
+                                    <li>our commitment</li>
+                                </a>
+                                <a href="#">
+                                    <li>environment</li>
+                                </a>
+                                <a href="#">
+                                    <li>sustainable fabrics</li>
+                                </a>
+                                <a href="#">
+                                    <li>people</li>
+                                </a>
+                            </ul>
+                            <img src="https://www.klopman.com/wp-content/uploads/2019/02/I1l9wObA-286x300.jpg" class="details-img" alt="">
+                        </div>
+                    </div>
+                </li>
+                <a href="#" class="nav-link">
+                    <li>
+                        MAGAZINE
+                    </li>
+                </a>
+                <a href="#" class="nav-link">
+                    <li>
+                        CONTACTS
+                    </li>
+                </a>
+            </ul>
+            <a href="" id="nav-btn-container">
+                <button class="nav-btn">Fabric Finder</button>
+            </a>
+        </nav>
+    </header>
+
+
+    <header class="header-mobile">
+        <nav class="mobile-nav">
+            <button class="hamburger"><i class="fa fa-bars"></i></button>
+            <img src="assets/img/logo-color.png" class="logo-mobile" alt="">
+            <div style="display: flex; flex-direction: column; align-items: center;">
+                <button class="profile-btn"><i class="fa fa-user"></i></button>
+                <button class="language">
+                    <div class="flag-img">
+                        <img src="https://www.klopman.com/wp-content/plugins/sitepress-multilingual-cms/res/flags/en.png" alt="">
+                    </div>
+                    English
+                </button>
+            </div>
+        </nav>
+    </header>
+    
+    <div class="hamburger-menu-container">
+        <div class="super-container">
+            <ul class="hamburger-menu">
+                <div class="dropdown-container">
+                    <li class="hamburger-link">
+                        <div class="link-container" style="display: flex; justify-content: space-between">
+                            <span>Corporate</span>
+                            <i class="fa fa-angle-down"></i>
+                        </div>
+                    </li>
+                    <div class="mobile-dropdown">
+                        <ul>
+                            <a href="#">
+                                <li>About us</li>
+                            </a>
+                            <a href="#">
+                                <li>History</li>
+                            </a>
+                            <a href="#">
+                                <li>Our plants</li>
+                            </a>
+                            <a href="#">
+                                <li>Manufacturing and certifications</li>
+                            </a>
+                            <a href="#">
+                                <li>Management</li>
+                            </a>
+                            <a href="#">
+                                <li>Our Partners</li>
+                            </a>
+                            <a href="#">
+                                <li>Careers</li>
+                            </a>
+                            <a href="#">
+                                <li>Kai Fabrics</li>
+                            </a>
+                        </ul>
+                    </div>
+                </div>
+                <div class="dropdown-container">
+                    <li class="hamburger-link">
+                        <div class="link-container" style="display: flex; justify-content: space-between">
+                            <span>Fabrics</span>
+                            <i class="fa fa-angle-down"></i>
+                        </div>
+                    </li>
+                    <div class="mobile-dropdown">
+                        <ul>
+                            <a href="#">
+                                <li>About us</li>
+                            </a>
+                            <a href="#">
+                                <li>History</li>
+                            </a>
+                            <a href="#">
+                                <li>Our plants</li>
+                            </a>
+                            <a href="#">
+                                <li>Manufacturing and certifications</li>
+                            </a>
+                            <a href="#">
+                                <li>Management</li>
+                            </a>
+                            <a href="#">
+                                <li>Our Partners</li>
+                            </a>
+                            <a href="#">
+                                <li>Careers</li>
+                            </a>
+                            <a href="#">
+                                <li>Kai Fabrics</li>
+                            </a>
+                        </ul>
+                    </div>
+                </div>
+                <div class="dropdown-container">
+                    <li class="hamburger-link">
+                        <div class="link-container" style="display: flex; justify-content: space-between">
+                            <span>Industries</span>
+                            <i class="fa fa-angle-down"></i>
+                        </div>
+                    </li>
+                    <div class="mobile-dropdown">
+                        <ul>
+                            <div class="dropdown-links-container">
+                                <div class="link-container dropdown-in-dropdown">
+                                    <li>Types</li>
+                                    <i class="fa fa-angle-down"></i>
+                                </div>
+                                <!-- <p class="test">hello</p> -->
+                                <div class="child-dropdown">
+                                    <ul>
+                                        <a href="#">
+                                            <li>Lightwork</li>
+                                        </a>
+                                        <a href="#">
+                                            <li>History</li>
+                                        </a>
+                                        <a href="#">
+                                            <li>Our plants</li>
+                                        </a>
+                                        <a href="#">
+                                            <li>Manufacturing and certifications</li>
+                                        </a>
+                                        <a href="#">
+                                            <li>Management</li>
+                                        </a>
+                                        <a href="#">
+                                            <li>Our Partners</li>
+                                        </a>
+                                        <a href="#">
+                                            <li>Careers</li>
+                                        </a>
+                                        <a href="#">
+                                            <li>Kai Fabrics</li>
+                                        </a>
+                                    </ul>
+                                </div>
+                            </a>
+                        </ul>
+                    </div>
+                </div>
+                <div class="dropdown-container">
+                    <li class="hamburger-link">
+                        <div class="link-container" style="display: flex; justify-content: space-between">
+                            <span>Sustainability</span>
+                            <i class="fa fa-angle-down"></i>
+                        </div>
+                    </li>
+                    <div class="mobile-dropdown">
+                        <ul>
+                            <a href="#">
+                                <li>Our Commitment</li>
+                            </a>
+                            <a href="#">
+                                <li>Environment</li>
+                            </a>
+                            <a href="#">
+                                <li>Sustainable Fabrics</li>
+                            </a>
+                            <a href="#">
+                                <li>People</li>
+                            </a>
+                        </ul>
+                    </div>
+                </div>
+                <a href="#">
+                    <li class="hamburger-link">
+                        <div class="link-container" style="display: flex; justify-content: space-between">
+                            <span>Magazine</span>
+                            <i class="fa fa-angle-right"></i>
+                        </div>
+                    </li>
+                </a>
+                <a href="#">
+                    <li class="hamburger-link">
+                        <div class="link-container" style="display: flex; justify-content: space-between">
+                            <span>Contact</span>
+                            <i class="fa fa-angle-right"></i>
+                        </div>
+                    </li>
+                </a>
+            </ul>
+        </div>
+    </div>
+
+
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <script>
+        let hamburger = document.querySelector('.hamburger');
+        let hamburger_menu = document.querySelector('.hamburger-menu-container');
+
+        hamburger.addEventListener('click', () => {
+            hamburger_menu.classList.toggle('show-menu');
+        })
+
+        let hamburger_link = document.querySelectorAll('.hamburger-link');
+
+        for(let i = 0; i < hamburger_link.length; i++) {
+            hamburger_link[i].addEventListener('click', () => {
+                $(hamburger_link[i]).next().closest('.mobile-dropdown').toggleClass('show-menu');
+                $(hamburger_link[i]).toggleClass('focus');
+            })
+        }
+
+        let first_child = document.querySelectorAll('.dropdown-in-dropdown');
+
+        for(let i = 0; i < first_child.length; i++) {
+            first_child[i].addEventListener('click', () => {
+                $(first_child[i]).next().closest('.child-dropdown').toggleClass('show-menu')
+            })
+        }
+    </script>
+</body>
+
+</html>
